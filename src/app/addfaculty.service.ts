806efb228f8803ef8c15e11f06478c8e0c1ae4fb
unknown <pubududulla@gmail.com>
2023-12-20 11:55:57 +0530

    new update

--- src/app/addfaculty.service.ts
+++ src/app/addfaculty.service.ts
@@ -1,15 +1,25 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddfacultyService {
-  private apiUrl = 'http://localhost:8000/faculty/create';
+  private apiUrl = 'http://localhost:8000/faculty';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  addfaculty(addfacultyData: any) {
-    return this.http.post('http://localhost:8000/faculty/create', addfacultyData);
+  addfaculty(data: any) {
+    return this.http.post(`${this.apiUrl}/create`, data);
+
+
   }
+
+// Example URL in the AddfacultyService
+updateFaculty(facultyId: string, data: any) {
+  return this.http.patch(this.apiUrl + '/update/' + facultyId, data);
+}
+
+  
 }
